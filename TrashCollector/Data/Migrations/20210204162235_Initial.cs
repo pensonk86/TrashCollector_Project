dff9485bfdc4e5034854c7b1e51972d27f9addec
Penson, Kelly <pensonk86@gmail.com>
2021-02-04 09:48:40 -0800

    Created GlobalRouting Class

---
+++ TrashCollector/Data/Migrations/20210204162235_Initial.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TrashCollector.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "92e72e45-e708-4a9f-be4f-f373daca7507", "246102a7-b9cb-4fdf-9f8f-58defc74a1b5", "Admin", "Admin" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "92e72e45-e708-4a9f-be4f-f373daca7507");
+        }
+    }
+}
